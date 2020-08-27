a2c192b81324cb881e2ca3fabfb633d1f111c8c0
FreekAlberti <federicoalberi96@gmail.com>
2020-08-27 15:41:24 +0200

    secondo esercizio

--- js/script.js
+++ js/script.js
@@ -15,3 +15,31 @@ if (palindroma == true) {
 } else {
   console.log("non palindroma");
 }
+
+/*
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+var scelta = prompt("scrivi pari o dispari");
+var numeroUtente = parseInt(prompt("inserisci un numero tra 1 e 5"));
+
+var numCpu = Math.floor(Math.random() * (5 - 1) + 1);
+
+var somma = numCpu + numeroUtente;
+var pariDispari = false;
+if (somma % 2 == 0) {
+  pariDispari = true;
+}
+console.log("il tuo numero é " + numeroUtente + ", il numero del computer é " + numCpu + ".");
+console.log("la somma é " + somma + ".");
+console.log("hai scelto " + scelta + ".");
+
+if ((pariDispari == true) && (scelta == "pari") || (pariDispari == false) && (scelta == "dispari")) {
+  console.log("ha vinto l'utente");
+} else {
+  console.log("ha vinto il computer");
+}
