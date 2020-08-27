347e6b881f448fe4550fa4ca011a3396a358fd1f
FreekAlberti <federicoalberi96@gmail.com>
2020-08-27 17:40:59 +0200

    esercizio corretto

--- js/script.js
+++ js/script.js
@@ -2,15 +2,22 @@
 //Creare una funzione per capire se la parola inserita è palindroma
 
 var parola = prompt("scrivi una parola");
+
+function parolaPalindroma(par) {
 var conteggio = 0;
 var palindroma = false;
-for (var i = parola.length - 1; i >= 0; i--) {
-  if (parola[i] == parola[conteggio]) {
-    palindroma =true;
+for (var i = par.length - 1; i >= 0; i--) {
+  if (par[i] == par[conteggio]) {
+    palindroma = true;
   }
   conteggio++;
 }
-if (palindroma == true) {
+return palindroma;
+}
+
+var palONonPal = parolaPalindroma(parola);
+
+if (palONonPal == true) {
   console.log("palindroma");
 } else {
   console.log("non palindroma");
@@ -27,18 +34,29 @@ Dichiariamo chi ha vinto.
 var scelta = prompt("scrivi pari o dispari");
 var numeroUtente = parseInt(prompt("inserisci un numero tra 1 e 5"));
 
-var numCpu = Math.floor(Math.random() * (5 - 1) + 1);
+function genNum(){
+  return Math.floor(Math.random() * (5 - 1) + 1);
+}
+
+var numCpu = genNum();
 
 var somma = numCpu + numeroUtente;
+
+function numPariDispari(num) {
 var pariDispari = false;
 if (somma % 2 == 0) {
   pariDispari = true;
 }
+return pariDispari;
+}
+
+var sommaPariDispari = numPariDispari(somma);
+
 console.log("il tuo numero é " + numeroUtente + ", il numero del computer é " + numCpu + ".");
 console.log("la somma é " + somma + ".");
 console.log("hai scelto " + scelta + ".");
 
-if ((pariDispari == true) && (scelta == "pari") || (pariDispari == false) && (scelta == "dispari")) {
+if ((sommaPariDispari == true) && (scelta == "pari") || (sommaPariDispari == false) && (scelta == "dispari")) {
   console.log("ha vinto l'utente");
 } else {
   console.log("ha vinto il computer");
